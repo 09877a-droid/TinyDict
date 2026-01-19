2ed12bcbebca36d2ee66eb1fbe345754f9d74678
suki <a2522156419@163.com>
2026-01-19 17:38:35 +0000

    Initial Commit

---
+++ TinyDict/TinyDictApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyDictApp.swift
+//  TinyDict
+//
+//  Created by #1 on 2026/1/19.
+//
+
+import SwiftUI
+
+@main
+struct TinyDictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
